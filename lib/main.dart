cf809f8b46dd4948492f380e9a32b1e8e4157078
Lichyo <lichyo003@gmail.com>
2023-06-17 14:20:18 +0800

    firebase cli setup

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,13 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'pages/home_page.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const LTC());
 }
 
---
+++ lib/services/firebase_task.dart
@@ -0,0 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class FirebaseTask {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  Future<void> createTask({user, task}) async{
+    _firestore.collection('$user').add(task);
+  }
+
+
+}
