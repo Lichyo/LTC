68352e36b317461866426cb847ee3379b9822db4
Lichyo <lichyo003@gmail.com>
2023-06-17 14:27:03 +0800

    import firebase storage

--- lib/database/task_database.dart
+++ lib/database/task_database.dart
@@ -1,11 +1,13 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class FirebaseTask {
+class TaskDatabase {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   Future<void> createTask({user, task}) async{
     _firestore.collection('$user').add(task);
   }
+
+
 
 
 }
