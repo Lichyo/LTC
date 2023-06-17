225c9eb0e7ffe0ad46321e8994cabf0e4c95dab0
Lichyo <lichyo003@gmail.com>
2023-06-17 11:14:00 +0800

    init firebase core

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,11 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'pages/home_page.dart';
 import 'package:record/record.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp();
   runApp(const LTC());
 }
 
